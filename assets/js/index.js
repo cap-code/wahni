33399b2438214952fe5a943541c3f848f613d256
cap-code <advaith007cube@gmail.com>
2021-09-18 00:49:50 +0530

    change the design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,20 @@
-const topsection = document.querySelector('.topsection')
-const logo = document.querySelector('.logoimg');
-const it = document.querySelector('.erp');
 const energy = document.querySelector('.energy');
-it.addEventListener('mouseover',e=>{
-    console.log('hi')
-    if(topsection.classList.contains('topsection') || topsection.classList.contains('topgreen')){
-        topsection.classList.remove('topgreen');
-        topsection.classList.remove('topsection')
-    }
-    topsection.classList.add('topblue');
-    logo.src='./assets/images/it_wahni.png'
+const it = document.querySelector('.it');
+
+energy.addEventListener('mouseover',evt=>{
+    energy.classList.remove('animateout')
+    energy.classList.add('animatein');
 })
-it.addEventListener('click',e=>{
-    console.log('hi');
-    if(topsection.classList.contains('topsection') || topsection.classList.contains('topgreen')){
-        topsection.classList.remove('topgreen');
-        topsection.classList.remove('topsection')
-    }
-    topsection.classList.add('topblue');
-    logo.src='./assets/images/it_wahni.png'
-
+it.addEventListener('mouseover',evt=>{
+    it.classList.remove('animateout')
+    it.classList.add('animatein');
 })
-energy.addEventListener('mouseover',e=>{
-    if(topsection.classList.contains('topsection') || topsection.classList.contains('topblue')){
-        topsection.classList.remove('topblue');
-        topsection.classList.remove('topsection')
-    }
-    topsection.classList.add('topgreen');
-    logo.src='./assets/images/green_wahni.png'
 
+energy.addEventListener('mouseout',evt=>{
+    energy.classList.remove('animatein')
+    energy.classList.add('animateout');
 })
-energy.addEventListener('click',e=>{
-    if(topsection.classList.contains('topsection') || topsection.classList.contains('topblue')){
-        topsection.classList.remove('topblue');
-        topsection.classList.remove('topsection')
-    }
-    topsection.classList.add('topgreen');
-    logo.src='./assets/images/green_wahni.png'
-
-    })
-
-    function findinnerWidth(){
-        if(window.innerWidth >1024){
-            return 1440;
-        }else if(window.innerWidth <=1024 && window.innerWidth > 768){
-            return 1024;
-        }else if(window.innerWidth <=768 && window.innerWidth > 425){
-            return 768;
-        }else{
-            return 425;
-        }
-    }
+it.addEventListener('mouseout',evt=>{
+    it.classList.remove('animatein')
+    it.classList.add('animateout');
+})
